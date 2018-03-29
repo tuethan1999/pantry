70e1da94892966f4020c0222e840dc84f26e5b38
Saad Mazhar <saadmazhar15@gmail.com>
2018-03-28 22:49:01 -0400

    Update scanner to include livestream

--- scanner.js
+++ scanner.js
@@ -1,4 +1,59 @@
-const Quagga = require('quagga').default;
+
+var _scannerIsRunning = false;
+
+function startScanner() {
+    Quagga.init({
+        inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.querySelector('#scanner-container'),
+            constraints: {
+                width: 480,
+                height: 320,
+                facingMode: "environment"
+            },
+        },
+        decoder: {
+            readers: [
+                "code_128_reader",
+                "ean_reader",
+                "ean_8_reader",
+                "code_39_reader",
+                "code_39_vin_reader",
+                "codabar_reader",
+                "upc_reader",
+                "upc_e_reader",
+                "i2of5_reader"
+            ],
+            debug: {
+                showCanvas: true,
+                showPatches: true,
+                showFoundPatches: true,
+                showSkeleton: true,
+                showLabels: true,
+                showPatchLabels: true,
+                showRemainingPatchLabels: true,
+                boxFromPatches: {
+                    showTransformed: true,
+                    showTransformedBox: true,
+                    showBB: true
+                }
+            }
+        },
+
+    }, function (err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+
+        // Set flag to is running
+        _scannerIsRunning = true;
+    });
+
 
 Quagga.init({
     inputStream : {
@@ -16,4 +71,13 @@ Quagga.init({
       }
       console.log("Initialization finished. Ready to start");
       Quagga.start();
-  });
+});
+
+// Start/stop scanner
+document.getElementById("btn").addEventListener("click", function () {
+    if (_scannerIsRunning) {
+        Quagga.stop();
+    } else {
+        startScanner();
+    }
+}, false);
