823830ce8624d4b700e120930996724f8110456a
Ethan TU <tuethan1999@gmail.com>
2018-03-28 23:19:28 -0400

    turned all the code into functions for database.js

--- database.js
+++ database.js
@@ -4,12 +4,17 @@ var product_id = '046838040597';
 var api_key = 'FD9DB913EB715F95377CCD3E21086606';
 var url = create_url(base_url, product_id, api_key);
 
+get_info(url);
+
 function create_url(base, id, key)
 {
 	return base + '/' + id + '/' + key;
 }
-request(url, function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+function get_info(complete_url)
+{
+	request(complete_url, function (error, response, body) {
+	  console.log('error:', error);
+	  console.log('statusCode:', response && response.statusCode); 
+	  console.log('body:', body); 
+	});	
+}
