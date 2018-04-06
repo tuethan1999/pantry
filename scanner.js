a513f58facf9891226e1ed9e7052a4ca638ccc3d
Saad Mazhar <saadmazhar15@gmail.com>
2018-04-06 16:40:55 -0400

    Added option to upload file for static barcode scanning

--- scanner.js
+++ scanner.js
@@ -1,6 +1,21 @@
 
 var _scannerIsRunning = false;
 
+var input = document.querySelector('input');
+var preview = document.querySelector('.preview');
+input.style.opacity = 0;
+
+input.addEventListener('change', updateImageDisplay);
+
+// Start/stop scanner
+document.getElementById("btn").addEventListener("click", function() {
+    if (_scannerIsRunning) {
+        Quagga.stop();
+    } else {
+        startScanner();
+    }
+}, false);
+
 function startScanner() {
     Quagga.init({
         inputStream: {
@@ -83,12 +98,67 @@ function startScanner() {
     });
 }
 
-// Start/stop scanner
-document.getElementById("btn").addEventListener("click", function() {
-        if (_scannerIsRunning) {
-            Quagga.stop();
-        } else {
-            startScanner();
-        }
-    }, false);
+function updateImageDisplay() {
+    while(preview.firstChild) {
+        preview.removeChild(preview.firstChild);
+    }
 
+    var curFiles = input.files;
+    if(curFiles.length === 0) {
+        var para = document.createElement('p');
+        para.textContent = 'No files currently selected for upload';
+        preview.appendChild(para);
+    } 
+    else {
+        var list = document.createElement('ol');
+        preview.appendChild(list);
+        for(var i = 0; i < curFiles.length; i++) {
+            var listItem = document.createElement('li');
+            var para = document.createElement('p');
+            if(validFileType(curFiles[i])) {
+                para.textContent = 'File name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
+                var image = document.createElement('img');
+                image.src = window.URL.createObjectURL(curFiles[i]);
+
+                listItem.appendChild(image);
+                listItem.appendChild(para);
+            } 
+            else {
+                para.textContent = 'File name ' + curFiles[i].name + ': Not a valid file type. Update your selection.';
+                listItem.appendChild(para);
+            }
+
+            list.appendChild(listItem);
+        }
+    }
+}
+
+
+
+function validFileType(file) {
+    var fileTypes = [
+        'image/jpeg',
+        'image/pjpeg',
+        'image/png'
+    ]
+        
+    for(var i = 0; i < fileTypes.length; i++) {
+        if(file.type === fileTypes[i]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function returnFileSize(number) {
+    if(number < 1024) {
+        return number + 'bytes';
+    } 
+    else if(number > 1024 && number < 1048576) {
+        return (number/1024).toFixed(1) + 'KB';
+    } 
+    else if(number > 1048576) {
+        return (number/1048576).toFixed(1) + 'MB';
+    }
+}
